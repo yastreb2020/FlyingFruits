11acdd415af9646c8750e033290f6791b3661e38
Katya Yastrebkova <katya2014ya@gmail.com>
2023-04-09 13:07:05 +0300

    spin fixed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 5;
     private float offsetX = -1;
     private int score = 0;
+    private bool spinActive = false;
+    public int step = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,15 +20,38 @@ public class PlayerController : MonoBehaviour
     {
         Movement();
 
+        if (Input.GetKeyDown(KeyCode.Space) && !spinActive)
+        {
+            spinActive = true;
+            StartCoroutine(Spin());
+        }
+
+        /*
         if (Input.GetKeyDown(KeyCode.Space))
         {
             transform.position += new Vector3(offsetX, 0, 0);
-            // transform.Rotate(Vector3.forward);// rotate 90 degrees on the z axis and then return back to 0
         } else if (Input.GetKeyUp(KeyCode.Space))
         {
             transform.position -= new Vector3(offsetX, 0, 0);
-            // transform.Rotate(Vector3.forward);// rotate 90 degrees on the z axis and then return back to 0
         }
+        */
+    }
+
+    // rotate 90 degrees on the z axis and then return back to 0
+    IEnumerator Spin()
+    {
+        for (int i = 0; i < 90; i += step)
+        {
+            transform.Rotate(Vector3.forward * step);
+            yield return null;
+        }
+
+        for (int i = 0; i < 90; i += step)
+        {
+            transform.Rotate(Vector3.forward * -step);
+            yield return null;
+        }
+        spinActive = false;
     }
 
     void Movement()
